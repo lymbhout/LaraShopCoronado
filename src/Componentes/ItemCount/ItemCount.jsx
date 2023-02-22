8806e81557adfeaf634cd0b1d5e04aaadd7b4cea
jose angel coronado <joseenan2777678@gmail.com>
2023-02-22 16:49:42 -0400

    incorporando funcion onAdd
    funcion que retorna la compra

--- src/Componentes/ItemCount/ItemCount.jsx
+++ src/Componentes/ItemCount/ItemCount.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-export const ItemCount = ({valorInicial,stock}) => {
+export const ItemCount = ({valorInicial,stock,onAdd}) => {
 
     const [contador,setcontador] = useState(valorInicial)
 
@@ -11,6 +11,7 @@ export const ItemCount = ({valorInicial,stock}) => {
             <button className='btn btn-dark' onClick={() =>restar()}>-</button>
             {contador}
             <button className='btn btn-dark' onClick={() =>sumar()}>+</button>
+            <button className='btn btn-light' onClick={()=> onAdd(contador)}>Agregar al carrito</button>
         </div>
     );
 }
--- src/Componentes/ItemDetail/ItemDetail.jsx
+++ src/Componentes/ItemDetail/ItemDetail.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { ItemCount } from '../ItemCount/ItemCount';
 export const ItemDetail = ({prods}) => {
+
+  const onAdd = (cantidad)=>{
+  console.log(cantidad);
+  }
+
     return (
       <div className="card mb-3" style={{maxWidth: '540px'}}>
   <div className="row g-0">
@@ -12,7 +17,7 @@ export const ItemDetail = ({prods}) => {
         <h5 className="card-title">{prods.nombre}</h5>
         <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
         <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-        <ItemCount valorInicial={1} stock={prods.stock}/> 
+        <ItemCount valorInicial={1} stock={prods.stock} onAdd={onAdd}/> 
         <button className='btn btn-dark'>finalizar compra</button>
       </div>
     </div>
